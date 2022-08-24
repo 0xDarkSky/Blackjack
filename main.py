340e5cdef6d291cdb7907e4a4e4cbd47f53aa793
blackpill0w <blackpill0w@protonmail.com>
2022-08-24 22:32:33 +0200

    Changed The type Function Name's To display & Some Styling

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import random
 import os
 import sys
 import time
-from termcolor import colored 
- 
-def type(x): 
+from termcolor import colored
+
+def display(x):
     for a in x:
         sys.stdout.write(a)
         sys.stdout.flush()
-        time.sleep(0.04) 
- 
+        time.sleep(0.04)
+
 def clear(): # clears terminal
     os.system('cls' if os.name == 'nt' else 'clear') # nt - windows, else linux/mac
- 
+
 suits = ("Clubs", "Hearts", "Diamonds", "Spades")
 ranks = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10")
 restart = "\n\nWanna play again? Y/n "
@@ -28,28 +28,29 @@ def main():
    player_bank = 0
    dealer_bank = 0
    while True:
-      ask = "\nHit or Stand?\n"
+      ask = '\n' + colored("H", attrs=['bold']) + 'it or ' \
+              + colored('S', attrs=['bold']) + 'tand?\n'
       print(ask)
       answer = input("")
-      get_suit = random.choice(suits) 
-   
-      if answer.lower() == "h":
-         type("\nPulling card for Player...\n\n")
+      get_suit = random.choice(suits)
+
+      if answer.lower() in ('h', 'hit'):
+         display("\nPulling card for Player...\n\n")
          get_card_player = random.choice(ranks)
          player_bank += int(get_card_player)
          time.sleep(1.5)
          if get_card_player == "1":
-            type(f"You got Ace of {get_suit}\n")
+            display(f"You got Ace of {get_suit}\n")
          if get_card_player == "10":
             nums = ("10", "Jack", "Queen", "King")
             tens = random.choice(nums)
-            type(f"You got {tens} of {get_suit}\n")
-         else: 
-            type(f"You got {get_card_player} of {get_suit}\n")
+            display(f"You got {tens} of {get_suit}\n")
+         else:
+            display(f"You got {get_card_player} of {get_suit}\n")
          if player_bank > 21:
-            type(f"You hit {player_bank}.\n{player_lost} ")
+            display(f"You hit {player_bank}.\n{player_lost} ")
             time.sleep(1)
-            type(restart)
+            display(restart)
             res = input("")
             if res.lower() == "y":
                os.system("clear")
@@ -57,41 +58,41 @@ def main():
             else:
                exit()
          if player_bank == 21:
-            type("\nPlayer hit 21! ")
-            type(f"\n{dealer_lost}")
+            display("\nPlayer hit 21! ")
+            display(f"\n{dealer_lost}")
             time.sleep(1)
-            type(restart)
+            display(restart)
             res = input("")
             if res.lower() == "y":
                os.system("clear")
                main()
             else:
                exit()
-         print(f"\nDealer cards: {dealer_bank} \nPlayer cards: {player_bank}") 
-   
-      elif answer.lower() == "s":
-         type("\nPulling card for Dealer...\n\n")
+         print(f"\nDealer cards: {dealer_bank} \nPlayer cards: {player_bank}")
+
+      elif answer.lower() in ('s', 'stand'):
+         display("\nPulling card for Dealer...\n\n")
          get_card_dealer = random.choice(ranks)
          def dealer():
             dealer = (f"Dealer gets {get_card_dealer} of {get_suit}\n")
-            type(dealer)
-         
+            display(dealer)
+
          dealer_bank += int(get_card_dealer)
          time.sleep(1.5)
          if get_card_dealer == "1":
-            type(f"Dealer got Ace of {get_suit}\n")
+            display(f"Dealer got Ace of {get_suit}\n")
          if get_card_dealer == "10":
             num = ("10", "Jack", "Queen", "King")
             ten = random.choice(num)
-            type(f"Dealer got {ten} of {get_suit}\n")
+            display(f"Dealer got {ten} of {get_suit}\n")
          else:
-            dealer() 
+            dealer()
          print(f"\nDealer cards: {dealer_bank} \nPlayer cards: {player_bank}")
          if dealer_bank >= 21:
-            type(f"\nDealer hit {dealer_bank} ")
-            type(f"\n{dealer_lost}")
+            display(f"\nDealer hit {dealer_bank} ")
+            display(f"\n{dealer_lost}")
             time.sleep(1)
-            type(restart)
+            display(restart)
             res = input("")
             if res.lower() == "y":
                os.system("clear")
@@ -99,10 +100,10 @@ def main():
             else:
                exit()
          if dealer_bank == 21:
-            type("\nDealer hit 21! ")
-            type(colored("\n\nDealer won! You lost!", "red", attrs=["blink"]))
+            display("\nDealer hit 21! ")
+            display(colored("\n\nDealer won! You lost!", "red", attrs=["blink"]))
             time.sleep(1)
-            type(restart)
+            display(restart)
             res = input("")
             if res.lower() == "y":
                os.system("clear")
@@ -110,10 +111,10 @@ def main():
             else:
                exit()
          if dealer_bank > float(player_bank) and dealer_bank < 22:
-            type("\nDealer hit more than you! ")
-            type(colored("\n\nDealer won! You lost!", "red", attrs=["blink"]))
+            display("\nDealer hit more than you! ")
+            display(colored("\n\nDealer won! You lost!", "red", attrs=["blink"]))
             time.sleep(1)
-            type(restart)
+            display(restart)
             res = input("")
             if res.lower() == "y":
                os.system("clear")
